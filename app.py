6ff6d6b345ef49e0e138108a84d9f8850982acab
musatf321 <musti.22@hotmail.de>
2021-04-15 01:45:07 +0200

    Put and delet method

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 from fastapi import FastAPI
 import uvicorn
-import json
+from pydantic import BaseModel
+app = FastAPI()
+db =[]
 
-#Init
-app = FastAPI(debug=True)
+class Measurement(BaseModel):
+    senorID: str
+    temperature : str
+    humidity : str
 
-with open("teperatur.json") as f:
-    teperatur = json.load(f)
+@app.get('/api/v1/measurements/measurment')
+async def measurements():
+    return db
 
+@app.get('/api/v1/measurements/{measurment_id}')
+async def get_measurement(measurment_id: int):
+    print(measurment_id)
+    return db[measurment_id] 
 
-@app.get('/api/v1/temperatur')
-async def get_temperatur():
-    return{"temperaur":teperatur}    
+@app.put('/api/v1/temperatures' )
+def add_tempetratur( measurement : Measurement):
+ db.append(measurement.dict())
+ return db[-1]
+
+@app.delete('/api/v1/measurements/{measurment_id}')
+async def delet_measurement(measurment_id: int):
+    print(measurment_id)
+    db.pop(measurment_id -1)
+    return {'measurement deleted'}
+
 
 if __name__ == '__main__':
     uvicorn.run(app,host="127.0.0.1",port="8080")    
+    
