67203feabadb8895b05acfd448882253bdb11c4b
Mustafa Al-Maawi <58593734+mustaf321@users.noreply.github.com>
2021-08-25 12:43:06 +0200

    Update reicve.ino

--- curent_code/reicve.ino
+++ curent_code/reicve.ino
@@ -10,18 +10,33 @@
 #define ss 15
 #define rst 16
 #define dio0 4
+#define SERVER_IP "192.168.2.124:8080"
+String temp1;
+String hum;
+String del;
 
-
-float t = 0.0;
-float h = 0.0;
 char jsonOutput[500];
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "WLAN-EUDSR4";
+const char* password = "9704142346724801";
 const size_t CAPACITY = JSON_OBJECT_SIZE(6);
 StaticJsonDocument<CAPACITY> doc; 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
+
+WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+
+
  
   Serial.println("LoRa Receiver Callback");
  
@@ -34,16 +49,74 @@ void setup() {
  
   // register the receive callback
   LoRa.onReceive(onReceive);
+
  
-  // put the radio into receive mode
+
   
 }
  
 void loop() {
   delay(3000);
+   // put the radio into receive mode
   LoRa.receive();
-  
-}
+ 
+   DynamicJsonDocument doc1(1024);
+  JsonObject object = doc.to<JsonObject>();
+  object["sensorID"] = "1";
+  object["temperature"] = temp1;
+  object["humidity"] = hum;
+  object["temperature2"] = del ;
+  serializeJsonPretty(doc, jsonOutput);
+Serial.print(jsonOutput);
+
+   if ((WiFi.status() == WL_CONNECTED)) {
+
+    WiFiClient client;
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+    // configure traged server and url
+    http.begin(client, "http://"SERVER_IP"/api/v1/temperatures"); //HTTP
+    http.addHeader("Content-Type", "application/json");
+
+    Serial.print("[HTTP] POST..." + String(jsonOutput) + "\n");
+    // start connection and send HTTP header and body
+   int httpCode = http.POST(jsonOutput);
+    
+    Serial.print("ICh bin hier!!!!!!!");
+    // httpCode will be negative on error
+    Serial.print(httpCode);
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] PUT Response code: %d\n", httpCode);
+      if (httpCode == 500) {http.end();
+        http.begin(client, "http://" SERVER_IP "/api/v1/temperatures"); //HTTP
+        http.addHeader("Content-Type", "application/json");
+
+        Serial.print("[HTTP] post..." + String(jsonOutput)+ "\n");
+        // start connection and send HTTP header and body
+        
+        Serial.printf("[HTTP] POST Response code: %d\n", httpCode);
+
+      }
+
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        const String& payload = http.getString();
+        Serial.println("received payload:\n<<");
+        Serial.println(payload);
+        Serial.println(">>");
+      }
+    } else {
+      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+
+    delay(10000);
+  }
+ }
 
 
 
@@ -88,20 +161,12 @@ Serial.println(LoRa.packetRssi());
 Serial.println("---------");
 
 
-String temp1 = String(String(res[18])+String(res[19])+String(res[20])+String(res[21])+String(res[22]));
-Serial.println(temp1);
-String hum = String(String(res[30])+String(res[31])+String(res[32])+String(res[33])+String(res[34]));
-Serial.println(hum);
-String del = String(String(res[44])+String(res[45])+String(res[46])+String(res[47])+String(res[48]));
-Serial.println(del);
+temp1 = String(String(res[19])+String(res[20])+String(res[21])+String(res[22])+String(res[23]));
+hum = String(String(res[31])+String(res[32])+String(res[33])+String(res[34])+String(res[35]));
+del = String(String(res[45])+String(res[46])+String(res[47])+String(res[48])+String(res[49]));
 
-DynamicJsonDocument doc1(1024);
-  JsonObject object = doc.to<JsonObject>();
-  object["sensorID"] = "1";
-  object["Temp:"] = temp1;
-  object["Hum:"] = hum;
-  object["Temp2:"] = del ;
-  serializeJsonPretty(doc, jsonOutput);
-Serial.print(jsonOutput);  
- 
+
+
+
+
 }
